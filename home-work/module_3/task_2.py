058e9a0a4eea7024d6615060deda2aace02f2e9d
mharnichev <mharnichev@gmail.com>
2026-03-26 13:57:12 +0200

    add

--- home-work/module_3/task_2.py
+++ home-work/module_3/task_2.py
@@ -29,7 +29,10 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000 or min > max or quantity < 1 or quantity > (max - min + 1): 
+    if (
+        min < 1 or max > 1000 or
+        min > max or quantity < 1 or 
+        quantity > (max - min + 1)): 
         return []
 
     unique_numbers = []
